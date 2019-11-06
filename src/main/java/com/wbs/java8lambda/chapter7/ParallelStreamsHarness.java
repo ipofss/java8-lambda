a724f3da4ada6b509cbe9abcca71bdf59a87b9d0
wangbingshuai <274694451@qq.com>
2019-11-06 14:55:32 +0800

    第7章示例代码

---
+++ src/main/java/com/wbs/java8lambda/chapter7/ParallelStreamsHarness.java
@@ -0,0 +1,39 @@
+package com.wbs.java8lambda.chapter7;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.function.Function;
+
+/**
+ * TODO
+ *
+ * @author: wangbingshuai
+ * @create: 2019-11-05 20:54
+ **/
+public class ParallelStreamsHarness {
+    public static final ForkJoinPool FORK_JOIN_POOL = new ForkJoinPool();
+
+    public static void main(String[] args) {
+        System.out.println("Iterative Sum done in: " + measurePerf(ParallelStreams::iterativeSum, 10_000_000L) + " msecs");
+        System.out.println("Sequential Sum done in: " + measurePerf(ParallelStreams::sequentialSum, 10_000_000L) + " msecs");
+        System.out.println("Parallel forkJoinSum done in: " + measurePerf(ParallelStreams::parallelSum, 10_000_000L) + " msecs");
+        System.out.println("Range forkJoinSum done in: " + measurePerf(ParallelStreams::rangedSum, 10_000_000L) + " msecs");
+        System.out.println("Parallel range forkJoinSum done in: " + measurePerf(ParallelStreams::parallelRangedSum, 10_000_000L) + " msecs");
+        System.out.println("ForkJoin sum done in: " + measurePerf(ForkJoinSumCalculator::forkJoinSum, 10_000_000L) + " msecs");
+        System.out.println("SideEffect sum done in: " + measurePerf(ParallelStreams::sideEffectSum, 10_000_000L) + " msecs");
+        System.out.println("SideEffect prallel sum done in: " + measurePerf(ParallelStreams::sideEffectParallelSum, 10_000_000L) + " msecs");
+    }
+
+    private static <T, R> long measurePerf(Function<T, R> f, T input) {
+        long fastest = Long.MAX_VALUE;
+        for (int i = 0; i < 10; i++) {
+            long start = System.nanoTime();
+            R apply = f.apply(input);
+            long duration = (System.nanoTime() - start) / 1_000_000;
+            System.out.println("Result:" + duration);
+            if (duration < fastest) {
+                fastest = duration;
+            }
+        }
+        return fastest;
+    }
+}
